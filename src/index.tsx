d86e3c6b533ffeea6192931d85f68e82eb08ea99
DanielJDupont <ddupont2@uwo.ca>
2021-02-21 20:16:20 -0500

    removed uneeded divs

--- src/index.tsx
+++ src/index.tsx
@@ -61,13 +61,11 @@ const Anime = forwardRef((props: AnimeProps, ref) => {
   const childArray = Array.isArray(props.children) ? props.children : [props.children];
 
   return (
-    <div>
-      <div className={props.className} style={{ display: 'flex', width: '1px', ...props.style }}>
-        {childArray.map((child) => (
-          // Wrap the user's elements with a div containing the __anime__ tag so we can apply the animations to them.
-          <div className="__anime__">{child}</div>
-        ))}
-      </div>
+    <div className={props.className} style={{ ...props.style }}>
+      {childArray.map((child) => (
+        // Wrap the user's elements with a div containing the __anime__ tag so we can apply the animations to them.
+        <div className="__anime__">{child}</div>
+      ))}
     </div>
   );
 });
